9244bd9528f65fd3e2adf4612c11a795c0e782b8
jingyi <zhao.elton@gmail.com>
2025-02-17 23:58:03 -0800

    - reorg project structure

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { Application } from '@/components/Application';
 import '@copilotkit/react-ui/styles.css';
 import '@radix-ui/themes/styles.css';
 import React from 'react';
-import { AuditProvider } from '@/lib/hooks/use-audit';
+import { AuditProvider } from './lib/hooks/use-audit';
 
 const RUNTIME_URL = process.env.NEXT_PUBLIC_COPILOT_RUNTIME_ENDPOINT;
 
